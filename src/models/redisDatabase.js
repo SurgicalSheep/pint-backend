3f88b21a96c9b5732ab600c88304a2f80988ef7f
SurgicalSheep <43825220+SurgicalSheep@users.noreply.github.com>
2022-07-13 17:54:55 +0100

    Update redisDatabase.js

--- src/models/redisDatabase.js
+++ src/models/redisDatabase.js
@@ -1,10 +1,6 @@
 const redis = require("redis");
 const client = redis.createClient({
-    url:process.env.REDIS_URL,
-    socket: {
-        tls: true,
-        rejectUnauthorized: false
-      }
+    url:process.env.REDIS_URL
 });
 
 (async()=>{
