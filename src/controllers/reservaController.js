b18a14635a9db17003c292b2dcf261379371bee1
SurgicalSheep <43825220+SurgicalSheep@users.noreply.github.com>
2022-07-17 12:50:12 +0100

    filter by ncolaborador

--- src/controllers/reservaController.js
+++ src/controllers/reservaController.js
@@ -9,9 +9,36 @@ const { searchNotificacaoSchema } = require("../schemas/reservaSchema");
 const Centro = require("../models/centro");
 const {sendUpdateReserva} = require('../helpers/sockets')
 
-const isValidDate = function(date) {
+/*const isValidDate = function(date) {
   return (new Date(date) !== "Invalid Date") && !isNaN(new Date(date));
-}
+}*/
+
+function isValidDate(dateString)
+{
+    // First check for the pattern
+    if(!/^\d{4}\-\d{1,2}\-\d{1,2}$/.test(dateString))
+        return false;
+
+    // Parse the date parts to integers
+    var parts = dateString.split("-");
+    var day = parseInt(parts[2], 10);
+    var month = parseInt(parts[1], 10);
+    var year = parseInt(parts[0], 10);
+    
+
+    // Check the ranges of month and year
+    if(year < 1000 || year > 3000 || month == 0 || month > 12)
+        return false;
+
+    var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+    // Adjust for leap years
+    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+        monthLength[1] = 29;
+
+    // Check the range of the day
+    return day > 0 && day <= monthLength[month - 1];
+};
 
 controllers.list = async (req, res) => {
   let { limit, offset,pesquisa,centros,mostrarAntingas} = req.query;
@@ -67,6 +94,53 @@ controllers.list = async (req, res) => {
         [Op.and]:[{
           [Op.or]:[{
             "$reservas.data$":new Date(pesquisa)
+          },{
+            "$utilizadore.nome$":{[Op.iLike]:"%"+pesquisa+"%"}
+          },{
+            "$sala.nome$":{[Op.iLike]:"%"+pesquisa+"%"}
+          }]
+        },{
+          "$sala.idcentro$":{[Op.in]: centros}
+        }]
+      }
+    });
+  }else if(pesquisa && !isNaN(pesquisa)){
+    data = await Reserva
+    .scope("noIdUtilizador")
+    .findAll({
+      limit: limit,
+      offset: offset,
+      include: [{ 
+        model: Sala,
+       }, { 
+        model: Utilizador.scope("noPassword"),
+      }],
+      where:{
+        [Op.and]:[{
+          [Op.or]:[{
+            "$utilizadore.ncolaborador$":pesquisa
+          },{
+            "$utilizadore.nome$":{[Op.iLike]:"%"+pesquisa+"%"}
+          },{
+            "$sala.nome$":{[Op.iLike]:"%"+pesquisa+"%"}
+          }]
+        },{
+          "$sala.idcentro$":{[Op.in]: centros}
+        }]
+        
+      },
+      order: [["data", "DESC"]],
+    });
+    count = await Reserva.count({
+      include: [{ 
+        model: Sala,
+       }, { 
+        model: Utilizador.scope("noPassword"),
+      }],
+      where:{
+        [Op.and]:[{
+          [Op.or]:[{
+            "$utilizadore.ncolaborador$":pesquisa
           },{
             "$utilizadore.nome$":{[Op.iLike]:"%"+pesquisa+"%"}
           },{
