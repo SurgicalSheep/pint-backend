590322692b4c90d310ce75bac3ae63546e3f491e
SurgicalSheep <43825220+SurgicalSheep@users.noreply.github.com>
2022-07-19 23:55:20 +0100

    Update centroSchema.js

--- src/schemas/centroSchema.js
+++ src/schemas/centroSchema.js
@@ -1,20 +1,20 @@
 const Joi = require("joi");
 
 const centroSchema = Joi.object({
-  nome: Joi.string().min(3).max(30).required(),
-  cidade: Joi.string().min(3).max(30).required(),
-  endereco: Joi.string().min(3).max(30).required(),
+  nome: Joi.string().min(3).required(),
+  cidade: Joi.string().min(3).required(),
+  endereco: Joi.string().min(3).required(),
   imagem: Joi.string().allow(null).optional(),
-  descricao: Joi.string().min(3).max(30),
+  descricao: Joi.string().min(3),
   estado: Joi.boolean()
 });
 
 const editcentroSchema = Joi.object({
-    nome: Joi.string().min(3).max(30).allow(null).optional(),
-    cidade: Joi.string().min(3).max(30).allow(null).optional(),
-    endereco: Joi.string().min(3).max(30).allow(null).optional(),
+    nome: Joi.string().min(3).allow(null).optional(),
+    cidade: Joi.string().min(3).allow(null).optional(),
+    endereco: Joi.string().min(3).allow(null).optional(),
     imagem: Joi.string().allow(null).optional(),
-    descricao: Joi.string().min(3).max(30).allow(null).optional(),
+    descricao: Joi.string().min(3).allow(null).optional(),
     estado: Joi.boolean().allow(null).optional()
   });
 
